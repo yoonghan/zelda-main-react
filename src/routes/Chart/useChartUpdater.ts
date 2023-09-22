39db57efb13b3ef0251dc73d7601275d10d24ab1
han <walcoorperation@gmail.com>
2023-09-23 02:21:00 +0800

    Add chart updater test

---
+++ src/routes/Chart/useChartUpdater.ts
@@ -0,0 +1,34 @@
+import { useReducer } from 'react'
+
+enum Actions {
+  APPEND,
+}
+
+interface Action {
+  type: Actions
+  value: number
+}
+
+function reducer(state: number[], action: Action) {
+  const { value, type } = action
+
+  switch (type) {
+    case Actions.APPEND:
+      return [...state, value]
+    default:
+      return state
+  }
+}
+
+export const useChartUpdater = (initialData = []) => {
+  const [data, dispatch] = useReducer(reducer, initialData)
+
+  const append = (value: number) => {
+    dispatch({ type: Actions.APPEND, value })
+  }
+
+  return {
+    data,
+    append,
+  }
+}
